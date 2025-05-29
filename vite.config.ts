d0d7aaefe9473ce9ab4c2078bfe69181540c2c74
Caleb Baker <4140348+bakerCaleb@users.noreply.github.com>
2025-05-29 11:16:41 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -19,12 +18,12 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-   build: {
+  build: {
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "index.html"),     // existing main site
         lovable: path.resolve(__dirname, "indexl.html"), // your added page
       },
     },
-   },
+  },
 }));

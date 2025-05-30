0a606b2b7f821e605af19fe5e4b63efcd3820f03
Caleb Baker <calebb@live.com>
2025-05-29 22:53:00 -0700

    Fix React Router with basename=/new, both sites working

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/new">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
